c120b02b37e46c18d6882822adeb0d0fda208662
sebi15 <sebi-moe@live.de>
2015-02-23 11:40:30 +0100

    ersterTaskVersuch

--- Taskboard/db/seeds.rb
+++ Taskboard/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create name: 'InBox'
+Category.create name: 'InProcess'
+Category.create name: 'InReview'
+Category.create name: 'Complete'
